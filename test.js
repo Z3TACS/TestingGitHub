b45468065c32693d6588aa72d511f1d68cc12ec3
Juan Ors <juan00conde@gmaill.com>
2022-03-31 10:11:00 +0200

    Arreglado 2

--- test.js
+++ test.js
@@ -1,7 +1,4 @@
 const aFunction = () => console.log("This is a Function");
 
-<<<<<<< HEAD
 const myFuncion = () => console.log("This is my Function");
-=======
 const hisFunction = () => console.log("This is my Function");
->>>>>>> his-branch
